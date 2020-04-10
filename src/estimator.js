168780e9625e1161e9ed35ef5448af9704a28243
Seun Odewale <seunodewale.zone5@gmail.com>
2020-04-11 00:21:32 +0100

    finished challenged 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,18 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const { reportedCases } = data;
+    let output = {
+        data,
+        impact: {},
+        severeImpact: {}
+    }
+
+    output.impact.currentlyInfected = reportedCases * 10;
+    output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * 1024; 
+
+    output.severeImpact.currentlyInfected = reportedCases * 50;
+    output.severeImpact.infectionsByRequestedTime = output.severeImpact.currentlyInfected * 1024;
+    
+    return output
+};
 
 export default covid19ImpactEstimator;
