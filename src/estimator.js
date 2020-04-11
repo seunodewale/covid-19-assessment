062505c59bbb236d290bca3bb1d440731a16c596
seunodewale <seunodewale@yahoo.com>
2020-04-11 04:08:28 +0100

    fixed infectionsByRequestedTime

--- src/estimator.js
+++ src/estimator.js
@@ -2,12 +2,13 @@ const covid19ImpactEstimator = (data) => {
   const { reportedCases } = data;
   const impact = {};
   const severeImpact = {};
+  const factor = 2 ** Math.trunc(30 / 3);
 
   impact.currentlyInfected = reportedCases * 10;
-  impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** 10;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * factor;
 
   severeImpact.currentlyInfected = reportedCases * 50;
-  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 2 ** 10;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * factor;
 
   return { data, impact, severeImpact };
 };
