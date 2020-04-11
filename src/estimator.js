5cb85b09637d2012446f168d4372b20098d332cb
seunodewale <seunodewale@yahoo.com>
2020-04-11 03:23:58 +0100

    fixed indentation

--- src/estimator.js
+++ src/estimator.js
@@ -1,19 +1,19 @@
 const covid19ImpactEstimator = (data) => {
-    const { reportedCases } = data;
-    let impact = {};
-    let severeImpact = {};
+  const { reportedCases } = data;
+  const impact = {};
+  const severeImpact = {};
 
-    impact.currentlyInfected = reportedCases * 10;
-    impact.infectionsByRequestedTime = impact.currentlyInfected * 512; 
+  impact.currentlyInfected = reportedCases * 10;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 512; 
 
-    severeImpact.currentlyInfected = reportedCases * 50;
-    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 512;
+  severeImpact.currentlyInfected = reportedCases * 50;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 512;
     
-    return {
-        data,
-        impact,
-        severeImpact
-    }
+  return {
+    data,
+    impact,
+    severeImpact
+  }
 };
 
 export default covid19ImpactEstimator;
