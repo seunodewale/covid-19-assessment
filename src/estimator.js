ce9d3cf04969f501d3f3cc816ef2175b6fd032d6
seunodewale <seunodewale@yahoo.com>
2020-04-11 14:30:29 +0100

    completed challenge 2

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,5 @@
 const covid19ImpactEstimator = (data) => {
-  const { reportedCases, timeToElapse, periodType } = data;
+  const { reportedCases, timeToElapse, periodType, totalHospitalBeds } = data;
   const impact = {};
   const severeImpact = {};
   let factor;
@@ -20,9 +20,13 @@ const covid19ImpactEstimator = (data) => {
 
   impact.currentlyInfected = reportedCases * 10;
   impact.infectionsByRequestedTime = impact.currentlyInfected * factor;
+  impact.severeCasesByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.15);
+  impact.hospitalBedsByRequestedTime = impact.severeCasesByRequestedTime - Math.trunc(totalHospitalBeds * 0.35);
 
   severeImpact.currentlyInfected = reportedCases * 50;
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * factor;
+  severeImpact.severeCasesByRequestedTime = Math.trunc(severeImpact.infectionsByRequestedTime * 0.15);
+  severeImpact.hospitalBedsByRequestedTime = severeImpact.severeCasesByRequestedTime - Math.trunc(totalHospitalBeds * 0.35);
 
   return { data, impact, severeImpact };
 };
