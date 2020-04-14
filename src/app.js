b3942f60b9f0857c0ad3ff0c7a817c3fa87d78ce
seunodewale <seunodewale@yahoo.com>
2020-04-14 13:17:29 +0100

    update logs

--- src/app.js
+++ src/app.js
@@ -10,10 +10,21 @@ import router from './router';
 const app = express();
 
 const port = process.env.PORT || 8000;
+const custom = (tokens, req, res) => {
+  const output = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    parseInt(tokens['response-time'](req, res), 10) < 10
+      ? `0${parseInt(tokens['response-time'](req, res), 10)}ms`
+      : `${parseInt(tokens['response-time'](req, res), 10)}ms`
+  ].join('\t\t');
+  return output;
+};
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, '../logs/access.txt'));
+const accessLogStream = fs.createWriteStream(path.join(__dirname, '../logs/log.txt'));
 
-app.use(logger(':method\t\t :url\t\t :status\t\t :response-time ms', { stream: accessLogStream }));
+app.use(logger(custom, { stream: accessLogStream }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,8 +36,6 @@ app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to this API.'
 }));
 
-app.listen(port, () => {
-  console.log(`Server is running on PORT ${port}`);
-});
+app.listen(port);
 
 export default app;
